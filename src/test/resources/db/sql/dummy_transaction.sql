ef749f9a21e0ec7093e570c04f07a2fbde7eeff7
Jonathan E <john.elkarat@gmail.com>
2018-08-26 02:10:13 +0300

    misc fixes

--- src/test/resources/db/sql/dummy_transaction.sql
+++ src/test/resources/db/sql/dummy_transaction.sql
@@ -1,8 +1,8 @@
 INSERT INTO TRANSACTION
-(from_account, to_account, transaction_type, amount, transaction_date, comments)
+(id, from_account, to_account, transaction_type, amount, transaction_date, comments)
 VALUES
-(NULL, 1, 'DEPOSIT', 500.5, '2018-01-19 03:14:07', 'Initial Deposit'),
-(NULL, 2, 'DEPOSIT', 100, '2018-01-19 03:14:07', 'Initial Deposit'),
-(NULL, 3, 'DEPOSIT', 400, '2018-01-19 03:14:07', 'Initial Deposit'),
-(1, NULL, 'WITHDRAWAL', 100, '2018-01-19 03:14:07', 'Withdrwal'),
-(3, 2, 'TRANSFER', 50.2, '2018-01-19 03:14:07', 'Nintendo Switch');
+(1, NULL, 1, 'DEPOSIT', 500.5, '2018-01-19 03:14:07', 'Initial Deposit'),
+(2, NULL, 2, 'DEPOSIT', 100, '2018-01-19 03:14:07', 'Initial Deposit'),
+(3, NULL, 3, 'DEPOSIT', 400, '2018-01-19 03:14:07', 'Initial Deposit'),
+(4, 1, NULL, 'WITHDRAWAL', 100, '2018-01-19 03:14:07', 'Withdrwal'),
+(5, 3, 2, 'TRANSFER', 50.2, '2018-01-19 03:14:07', 'Nintendo Switch');
